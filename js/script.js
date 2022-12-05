285b8416db692c6e3a894a6ed2600d911888c4b8
Prarthana Shevatekar <107344416+PrarthanaSh@users.noreply.github.com>
2022-12-04 21:28:39 -0500

    Added javascript code

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let menu = document.querySelector('nav ul');
+let hamburger = document.querySelector('nav .burgermenu');
+
+hamburger.addEventListener('click',function(){
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+      } else {
+        menu.style.display = "none";
+      }
+});
+
+
+let form = document.getElementById("contactForm");
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+  }).then(response => {
+      if (response.ok) {
+          status.innerHTML = "Your message was submitted! Please give me a day or two to come back to you.";
+          form.reset()
+      } else {
+          response.json().then(data => {
+              if (Object.hasOwn(data, 'errors')) {
+                  status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+              } else {
+                  status.innerHTML = "Oh no! There was a problem submitting your form."
+              }
+          })
+      }
+  }).catch(error => {
+      status.innerHTML = "Oh no! There was a problem submitting your form"
+  });
+}
+form.addEventListener('submit', handleSubmit);
